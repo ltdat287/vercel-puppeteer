45b2dd59270af4e168a95d64e27db2d134746567
datlt-cmc <ltdat4@cmc.com.vn>
2021-06-25 15:45:39 +0700

    remove mkdir public folder

--- api/run.js
+++ api/run.js
@@ -1,17 +1,11 @@
-import fs from 'fs';
-import path from 'path';
 import { screenshot } from '../browser';
 
 const handler = async (req, res) => {
   const { url = 'https://www.google.com.vn/' } = req.query;
 	console.log('request.query', req.query);
-	await fs.promises.mkdir('public', { recursive: true });
-	await fs.promises.writeFile('public/index.html', '<img src="/image.png">');
-
 	await screenshot(url);
 
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	fs.createReadStream(path.resolve(__dirname, '../public/index.html')).pipe(res);
+	res.send('<html><body><img src="/image.png"></body></html>');
 };
 
 module.exports = handler;
